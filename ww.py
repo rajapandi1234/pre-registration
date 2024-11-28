969ec40573b4df2f65ef26a521cb2ef47e2537f0
rajapandi1234 <138785181+rajapandi1234@users.noreply.github.com>
2024-11-28 15:08:48 +0530

    Create ww.py
    Signed-off-by: rajapandi1234 <138785181+rajapandi1234@users.noreply.github.com>

---
+++ ww.py
@@ -0,0 +1,4 @@
+# Mock API keys and secrets for testing purposes
+API_KEY = "1234567890abcdef1234567890abcdef"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+DB_PASSWORD = "SuperSecretPassword123!"
